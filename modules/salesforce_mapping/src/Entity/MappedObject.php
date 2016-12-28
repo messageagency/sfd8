75dcdac752b2198c9faa67deb5b66a3beb3cd9e2
Aaron Bauman <aaron@messageagency.com>
2016-12-28 15:48:54 -0500

    by aaronbauman - Add unique keys to MappedObject table on entity_type-entity_id-mapping

--- modules/salesforce_mapping/src/Entity/MappedObject.php
+++ modules/salesforce_mapping/src/Entity/MappedObject.php
@@ -22,6 +22,7 @@ use Drupal\salesforce_mapping\SalesforcePushEvent;
  *   label = @Translation("Salesforce Mapped Object"),
  *   module = "salesforce_mapping",
  *   handlers = {
+ *     "storage_schema" = "Drupal\salesforce_mapping\MappedObjectStorageSchema",
  *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
  *     "list_builder" = "Drupal\salesforce_mapping\MappedObjectList",
  *     "form" = {
