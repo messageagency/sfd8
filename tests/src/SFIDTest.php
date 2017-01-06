989de466332682d8e630288bc47cd3e1d4b520c2
git <git@787980.no-reply.drupal.org>
2017-01-06 12:17:34 -0500

    Adds basic objects tests
    - PullQueueItemTest, SFIDTest, SObjectTest
    - fixes mapping reference error in PullQueueItem constructor
    - fixes array init error in Object constructor

---
+++ tests/src/SFIDTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Drupal\Tests\salesforce;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\salesforce\SFID;
+use Drupal\salesforce\Exception;
+
+/**
+ * Test Object instantitation
+ *
+ * @group salesforce_pull
+ */
+
+class SFIDTest extends UnitTestCase {
+  static $modules = ['salesforce'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * Test object instantiation with good ID
+   */
+  public function testGoodID() {
+    $sfid = new SFID('1234567890abcde');
+    $this->assertTrue($sfid instanceof SFID);
+  }
+
+  /**
+   * Test object instantiation with bad ID
+   * @expectedException Exception
+   */
+  public function testBadID() {
+    $sfid = new SFID('1234567890');
+  }
+
+  /**
+   * Test object instantiation with bad ID
+   */
+  public function testConvertId() {
+    $sfid = new SFID('1234567890adcde');
+    $this->assertEquals('1234567890adcdeAAA', $sfid);
+  }
+}
