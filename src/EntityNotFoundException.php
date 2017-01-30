d1a37333a1ef1ee57d1eb904f8e717c393bed946
Aaron Bauman <aaron@messageagency.com>
2017-01-30 17:17:11 -0500

    testable exception, per Rhodes's comment

--- src/EntityNotFoundException.php
+++ src/EntityNotFoundException.php
@@ -2,18 +2,22 @@
 
 namespace Drupal\salesforce;
 
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
 /**
  * EntityNotFoundException extends Drupal\salesforce\Exception
  * Thrown when a load operation returns no results.
  */
 class EntityNotFoundException extends \RuntimeException {
 
+  use StringTranslationTrait;
+
   protected $entity_properties;
 
   protected $entity_type_id;
 
   public function __construct($entity_properties, $entity_type_id, Throwable $previous = NULL) {
-    parent::__construct(t('Entity not found. type: %type properties: %props', ['%type' => $entity_type_id, '%props' => var_export($entity_properties, TRUE)]), 0, $previous);
+    parent::__construct($this->t('Entity not found. type: %type properties: %props', ['%type' => $entity_type_id, '%props' => var_export($entity_properties, TRUE)]), 0, $previous);
     $this->entity_properties = $entity_properties;
     $this->entity_type_id = $entity_type_id;
   }
