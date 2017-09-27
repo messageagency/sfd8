3d7194b3e1e49ca13bbb0e0b8abd8217fa57148c
Aaron Bauman <aaron@messageagency.com>
2017-09-27 11:02:56 -0400

    ading example mapping type plugin to example module

---
+++ modules/salesforce_example/src/Plugin/SalesforceMappingField/Hardcoded.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\salesforce_example\Plugin\SalesforceMappingField;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\salesforce_mapping\SalesforceMappingFieldPluginBase;
+use Drupal\salesforce_mapping\Entity\SalesforceMappingInterface;
+use Drupal\salesforce_mapping\MappingConstants;
+
+/**
+ * Adapter for entity Constant and fields.
+ *
+ * @Plugin(
+ *   id = "hardcoded",
+ *   label = @Translation("Hardcoded Value")
+ * )
+ */
+class Hardcoded extends SalesforceMappingFieldPluginBase {
+
+  /**
+   *
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $pluginForm = parent::buildConfigurationForm($form, $form_state);
+
+    $pluginForm['drupal_field_value'] += [
+      '#type' => 'textfield',
+      '#default_value' => 'Hardcoded value',
+      '#description' => $this->t('This is a hardcoded value.'),
+      '#disabled' => TRUE,
+    ];
+
+    // @TODO: "Constant" as it's implemented now should only be allowed to be set to "Push". In the future: create "Pull" logic for constant, which pulls a constant value to a Drupal field. Probably a separate mapping field plugin.
+    $pluginForm['direction']['#options'] = [
+      MappingConstants::SALESFORCE_MAPPING_DIRECTION_DRUPAL_SF => $pluginForm['direction']['#options'][MappingConstants::SALESFORCE_MAPPING_DIRECTION_DRUPAL_SF],
+    ];
+    $pluginForm['direction']['#default_value'] =
+      MappingConstants::SALESFORCE_MAPPING_DIRECTION_DRUPAL_SF;
+
+    return $pluginForm;
+
+  }
+
+  /**
+   *
+   */
+  public function value(EntityInterface $entity, SalesforceMappingInterface $mapping) {
+    return 'Hardcoded value';
+  }
+
+  /**
+   *
+   */
+  public function pull() {
+    return FALSE;
+  }
+
+  // @TODO add validation handler (?)
+
+}
