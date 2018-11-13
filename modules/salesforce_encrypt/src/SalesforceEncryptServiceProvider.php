05eb0eb4c341575b5173ea554e6f76ae8478ec21
Aaron Bauman <aaron@messageagency.com>
2018-11-13 15:55:03 -0500

    Remove ununsed 'use'

--- modules/salesforce_encrypt/src/SalesforceEncryptServiceProvider.php
+++ modules/salesforce_encrypt/src/SalesforceEncryptServiceProvider.php
@@ -4,8 +4,6 @@ namespace Drupal\salesforce_encrypt;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
-use Drupal\salesforce_encrypt\Rest\RestClient;
-use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Modifies the salesforce client service.
