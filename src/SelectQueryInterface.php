d7e60f120844253773c51974fc884605434db714
Colin Corrigan <colin@messageagency.com>
2019-01-24 14:13:13 -0500

    Interface EOD

--- src/SelectQueryInterface.php
+++ src/SelectQueryInterface.php
@@ -15,4 +15,42 @@ interface SelectQueryInterface {
    */
   public function __toString();
 
+  /**
+   * Add a condition to the query.
+   *
+   * Valid operators include '=', '!=', '<', '<=', '>', '>=', 'LIKE', 'IN',
+   * 'NOT IN', 'INCLUDES', 'EXCLUDES'
+   *
+   * @param $field
+   * @param mixed|null $value
+   * @param string $operator
+   *
+   * @return mixed
+   */
+  public function condition($field, $value = NULL, $operator = '=');
+
+  public function where();
+
+  public function with();
+
+  public function limit($number_of_rows);
+
+  public function groupBy($field);
+
+  /**
+   * Sets sort order for
+   *
+   * @param string $field
+   * Salesforce field to order by.
+   * @param string $direction
+   *
+   * @param string $nulls
+   *
+   * @return mixed
+   */
+  public function orderBy($field, $direction = 'ASC', $nulls = 'FIRST');
+
+  public function offset($number_of_rows_to_skip);
+
+  public function having();
 }
